e55e376be643b0009d10296facee475f500ac30b
Lucasgdev <lenis@sga.pucminas.br>
2023-07-28 11:49:42 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#Importando Framework WEB
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+#Renominando o Flask
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@socketio.on("message")
+def gerenciar_mensagem(mensagem):
+    send(mensagem, broadcast=True)
+
+
+#Criando rota homepage
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+#iniciar o app
+socketio.run(app, host="192.168.100.106")
